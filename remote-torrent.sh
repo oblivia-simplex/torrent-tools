752ba1adf3772397fd72e63019e75293475a079c
Lucca Fraser <lucca.fraser@gmail.com>
2021-12-23 19:24:27 -0400

    ready

---
+++ remote-torrent.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+CATEGORY="$1"
+TORRENT="$2"
+
+DOWNLOAD_DIR="~/TRANSMISSION_DOWNLOADS/${CATEGORY}"
+
+REMOTE=user@basedmiles
+
+PORT=$(( $RANDOM % 50000 + 1024 ))
+
+echo "[+] We're going to use port $PORT"
+
+if [ -f "$2" ] ; then
+  echo "[+] $TORRENT is a file, behaving accordingly..."
+  REMOTE_TORRENT_LOCATION="/tmp/$(basename TORRENT)"
+  scp $TORRENT $REMOTE:"${REMOTE_TORRENT_LOCATION}"
+  ssh $REMOTE "mkdir -p ${DOWNLOAD_DIR} && transmission-cli -p $PORT -w ${DOWNLOAD_DIR} -f ~/bin/transmission-finisher.sh ${REMOTE_TORRENT_LOCATION}"
+else # assume magnet url
+  echo "[+] Assuming $TORRENT is a magnet link, behaving accordingly..."
+  MAGNET="$TORRENT"
+  ssh $REMOTE "mkdir -p ${DOWNLOAD_DIR} && transmission-cli -p $PORT -w ${DOWNLOAD_DIR} -f ~/bin/transmission-finisher.sh \"${MAGNET}\""
+fi
+  
--- transmission-finisher.sh
+++ transmission-finisher.sh
@@ -6,11 +6,21 @@
 # of the torrent dir
 ## 
 
+[ -n "${TR_TORRENT_DIR}" ] || { echo "TR_TORRENT_DIR not defined"; exit 1; }
+[ -n "${TR_TORRENT_NAME}" ] || { echo "TR_TORRENT_NAME not defined"; exit 1; }
+
 SUBDIR=$(basename ${TR_TORRENT_DIR})
 DESTDIR="/${SUBDIR}"
 
 echo "$(date): Finished torrenting ${TR_TORRENT_NAME}, moving from ${TR_TORRENT_DIR}/${TR_TORRENT_NAME} to ${DESTDIR}/${TR_TORRENT_NAME}" \
   >> ~/transmission-finisher.log
 
-mv -v ${TR_TORRENT_DIR}/${TR_TORRENT_NAME} ${DESTDIR}/${TR_TORRENT_NAME}
+mv -v "${TR_TORRENT_DIR}/${TR_TORRENT_NAME}" "${DESTDIR}/${TR_TORRENT_NAME}"
+
+echo "Attempting to kill parent process..."
+
+kill $PPID
+
+
+
 
